f8916cd8bd60f508057614158ab8ab75fdf6cc46
eswhik <96600749+eswhik@users.noreply.github.com>
2022-01-29 17:10:14 -0500

    Update key-alert.js

--- assets/scripts/key-alert.js
+++ assets/scripts/key-alert.js
@@ -1,14 +1,1 @@
-Swal.fire({
-            title:'Error Server',
-            icon:'error',
-            confirmButtonText:'Buy Version Vip',
-            backdrop:true,
-            position:'center',
-            confirmButtonColor:'#0073ff',
-            confirmButtonAriaLabel:'Aceptar',
-            allowOutsideClick:false,
-            allowEscapeKey:false,
-            stopKeydownPropagation:false
-        }).then(function () {
-          window.location.href = "https://es.hackersteam.xyz/volt/login";
-        })
+
